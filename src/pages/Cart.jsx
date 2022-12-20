3bf4244f65e0110174d3508a7287cc4da61f07be
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2022-12-20 12:45:15 +0700

    clean up

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import { Add, Remove } from "@material-ui/icons"
 import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
 import Announcement from "../components/Announcement"
 import Footer from "../components/Footer"
